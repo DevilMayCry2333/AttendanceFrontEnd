9b47d4b3041c03d7c0bb641c35400c62a24e6f8c
DevilMayCry2333 <1712363499@qq.com>
2019-06-27 15:49:43 +0800

    zuix

--- url.js
+++ url.js
@@ -1,7 +1,7 @@
 
-var globalUrl = "https://youkaiyu.com:8445/unnamed/";
-//var globalUrl = "http://localhost:/unnamed/";
+// var globalUrl = "https://youkaiyu.com:8445/unnamed/";
+var globalUrl = "http://localhost:8080/unnamed/";
 
-var globalFUrl = "https://youkaiyu.com/AttendanceFrontEnd/";
-//var globalFUrl = "http://youkaiyu.com/AttendanceFrontEnd/";
+// var globalFUrl = "https://youkaiyu.com/AttendanceFrontEnd/";
+var globalFUrl = "http://youkaiyu.com/AttendanceFrontEnd/";
 
