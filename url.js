b2ab00ea33787a37c9eec4a72ee0f5617a4e9484
DevilMayCry2333 <1712363499@qq.com>
2019-06-27 15:38:00 +0800

    最新

--- url.js
+++ url.js
@@ -1,2 +1,7 @@
-var globalFUrl = "http://localhost/AttendanceFrontEnd/";
-var globalUrl = "http://localhost:9080/AttendanceBackEnd_war_exploded/";
+
+var globalUrl = "https://youkaiyu.com:8445/unnamed/";
+//var globalUrl = "http://localhost:/unnamed/";
+
+var globalFUrl = "https://youkaiyu.com/AttendanceFrontEnd/";
+//var globalFUrl = "http://youkaiyu.com/AttendanceFrontEnd/";
+
