36c843330f36bf04dad68294520d3c2594ce6202
DevilMayCry2333 <1712363499@qq.com>
2019-07-02 10:59:45 +0800

    最新

--- url.js
+++ url.js
@@ -2,6 +2,6 @@
 // var globalUrl = "https://youkaiyu.com:8445/unnamed/";
 var globalUrl = "http://localhost:8080/unnamed/";
 
-// var globalFUrl = "https://youkaiyu.com/AttendanceFrontEnd/";
-var globalFUrl = "http://youkaiyu.com/AttendanceFrontEnd/";
+var globalFUrl = "http://localhost/AttendanceFrontEnd/";
+// var globalFUrl = "http://youkaiyu.com/AttendanceFrontEnd/";
 
